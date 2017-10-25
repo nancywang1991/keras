46c802bba0bfe9059b2fdf1ad12422734a13c7c5
Nancy Wang <nancywang1991@yahoo.ca>
2017-10-25 14:15:59 -0700

    ecog reg fix

--- keras/preprocessing/ecog_reg_xy.py
+++ keras/preprocessing/ecog_reg_xy.py
@@ -382,7 +382,7 @@ def extract_batch_y(self, index_array, start_time):
             ydata_start[1] = ydata_start[1] * (64 / 480.0)
             #mvmt = makeGaussian(8, center=abs(ydata_end-ydata_start))
             #batch_y[f] = np.ndarray.flatten(mvmt)
-            batch_y = np.sum(np.abs(ydata_end-ydata_start))
+            batch_y[f] = np.sum(np.abs(ydata_end-ydata_start))
         # batch_y[f,0] = mvmt
         except:
             pdb.set_trace()
